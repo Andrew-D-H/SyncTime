65a9628d8bc97f2190ad6840fe6c3c6bcc1ff2a2
willmess <47167135+willmess@users.noreply.github.com>
2025-05-12 18:55:20 -0700

    1. Added profile_page.xml 2. Added ProfilePageActivity.kt 3. Edit build.gradle.kts to include "implementation("androidx.appcompat:appcompat:1.6.1")"
    I added the basic profile page. Only Displays a picture of Android Robot. Does not display any information yet. The .kt file will give profile page functionality.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
---
+++ app/src/main/java/com/example/synctime/ProfilePageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.synctime
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfilePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_page)
+    }
+}
